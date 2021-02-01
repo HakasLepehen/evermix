9f59b7c57976e7560e9fc8b3e979f7c25ea4d840
HakasLepehen <doctorrosen2016@gmail.com>
2021-02-01 10:24:04 +0600

    clear project

--- README.md
+++
@@ -1,3 +0,0 @@
-<h1>Project "Evermix"</h1>
-
-<span>This is a training project of one of the sites in the Web, designed to check hard-skills of author</span>
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles/styles.scss';
-import './assets/img/call-img.svg';
-import './assets/img/basket-img.svg';
-import './assets/img/comparsion.svg';
-import './assets/img/slide-item.jpg';
-import './assets/img/search_icon.svg';
-import './assets/fonts/Montserrat/Montserrat-Regular.ttf';
-
-console.log('Index.js was loaded');
