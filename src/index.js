ade3595e1394a28287103f6370e160dae1f22ba1
Pavel Petrov <doctorrosen2016@gmail.com>
2021-03-03 00:42:54 +0600

    working with google-map

--- src/index.js
+++ src/index.js
@@ -13,7 +13,48 @@ import './assets/img/home_special_solutions.png';
 import './assets/img/black_right_arrow.svg';
 import './assets/img/calculation.svg';
 import './assets/img/question.svg';
+import {Loader} from "@googlemaps/js-api-loader";
 
+function Marker(lat, lng) {
+    this.lat = lat;
+    this.lng = lng;
+}
+
+let map;
+let markers = [
+    new Marker(56.7678557, 60.8027013),
+    new Marker(56.8408178, 60.5787715),
+    new Marker(56.8403162, 60.6220571),
+    new Marker(56.8440169, 60.6415134),
+    new Marker(56.8364945, 60.6134046),
+    new Marker(56.7960052, 60.6363833),
+
+];
+const loader = new Loader({
+    apiKey: 'AIzaSyBOXjzORCt4qtazCEkxJqXyajzsMnUcnyU',
+    version: 'week',
+});
+loader.load().then(() => {
+    map = new google.maps.Map(document.querySelector('.map-wrapper'), {
+        center: { lat: 56.8403092, lng: 60.597135 },
+        zoom: 12,
+    });
+
+    markers.forEach((el) => {
+        new google.maps.Marker({
+            position: el,
+            map,
+            title: ""
+        })
+    })
+})
+
+//Екатеринбург, пер. Речной, 1, литер И  56.7678557,60.8027013
+// ул Московская,д.1  56.8408178,60.5787715
+// Проспект Ленина, д.52/2  56.8403162,60.6220571
+// Проспект Ленина, д.101/2  56.8440169,60.6415134
+// ул.Малышева, 53, офис 452  56.8364945,60.6134046
+// ул. Белинского, 256, оф.452  56.7960052,60.6363833
 
 
 console.log('Index.js was loaded');
